6b51496f00b15268a49508bbd21fdc408ca48a66
gomathibalasubramaniam <47185635+gomathibalasubramaniam@users.noreply.github.com>
2019-05-18 19:11:03 +0530

    Create filghtcal.js

---
+++ filghtcal.js
@@ -0,0 +1,13 @@
+noOfFlightsTakeOff=100
+noOfFlightsLanded=110
+seatingCapacityTakeOff=150
+seatingCapacityLanded=185
+totalCookies=0
+totalCookies=(2*noOfFlightsTakeOff*seatingCapacityTakeOff)+(2*noOfFlightsLanded*(seatingCapacityLanded/2))
+
+//Write your code here
+//Populate the variable: totalCookies
+
+
+//Do not modify the below print statement for verification to work
+console.log(totalCookies)
