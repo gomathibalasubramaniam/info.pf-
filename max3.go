291f8ec879fd9a2040ba5a7f83d7cbb275091328
gomathibalasubramaniam <47185635+gomathibalasubramaniam@users.noreply.github.com>
2019-05-18 19:18:42 +0530

    Create max3.go

---
+++ max3.go
@@ -0,0 +1,14 @@
+package main
+
+func main(){
+    var num1 int=3
+    var num2 int=4
+    var num3 int=1
+    if (num1>num2 && num1>num3){
+        println(num1)
+    }else if(num2>num3){
+        println(num2)
+        
+    }else{ 
+        println(num3)}
+}
